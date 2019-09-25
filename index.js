96ffd0afc914cb13988a83865b0d840e1f3b343c
essential-hacker-4489 <akpojotor@yahoo.com>
2019-09-25 20:30:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ function sayHiToGrandma(string){
     return "I love you, too!"
     
   }
-  if(string ===string.toUpperCase()){
+  if(string ===string.toLowerCase()){
     return "I can't hear you!"
+  }
+  if(string ===string.toUpperCase()){
+    return "YES INDEED"
   }
 }
